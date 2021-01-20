f2a63f2e2fc4f11eba95c3ec56c372ff1fe45491
adamdkohler@gmail.com <Babysounder3!>
2021-01-19 18:45:56 -0800

    Event and EventDao set up

---
+++ RoomDBSample/app/src/main/java/com/example/roomdbexample/roomdb/Event.java
@@ -0,0 +1,30 @@
+package com.example.roomdbexample.roomdb;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Event {
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    public int _id;
+
+    @ColumnInfo(name = "title")
+    public String title;
+
+    @ColumnInfo(name = "date")
+    public String date;
+
+    @ColumnInfo(name = "img_id")
+    public int img_id;
+
+
+     public Event() {}
+
+     public Event (String title, String data, int imgId) {
+         this.title = title;
+         this.date = data;
+         this.img_id = imgId;
+     }
+}
