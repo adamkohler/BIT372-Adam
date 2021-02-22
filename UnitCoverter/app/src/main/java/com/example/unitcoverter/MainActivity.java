5d72182bdcef1dd3f3fff509e786c1de3db668de
adamdkohler@gmail.com <Babysounder3!>
2021-02-22 10:47:26 -0800

    mutablelivedata created, textviewmodel and observer used.

--- UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.unitcoverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -11,22 +15,34 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextViewModel textviewmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button fbtn = findViewById(R.id.convert_btn);
+        EditText finput = findViewById(R.id.conversion_input);
+        TextView ctxt = findViewById(R.id.converted_text);
+
+        textviewmodel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TextViewModel.class);
+        final Observer<Double> textObserver = new Observer<Double>() {
+            @Override
+            public void onChanged(Double s) {
+                if (!(s.toString().length() == 0))
+                    ctxt.setText(s.toString());
+            }
+        };
+        textviewmodel.getCurrentInput().observe(this, textObserver);
 
         Spinner spinner = (Spinner) findViewById(R.id.conversion_selection);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.conversionChoices, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-
-        Button fbtn = findViewById(R.id.convert_btn);
-        EditText finput = findViewById(R.id.conversion_input);
-        TextView ctxt = findViewById(R.id.converted_text);
 
         fbtn.setOnClickListener(v -> {
             if (finput.getText().length() == 0)
@@ -53,5 +69,12 @@ public class MainActivity extends AppCompatActivity {
             }
             }
         });
+
+        /*if (finput.getText().length() == 0){}
+        else {
+            //textviewmodel.getCurrentInput().setValue(Converter.toCelcius(Float.parseFloat(finput.getText().toString())));
+            double celcius = Converter.toCelcius(Float.parseFloat(finput.getText().toString()));
+            ctxt.setText(String.format("%.2f ºC", celcius));
+        }*/
     }
 }
